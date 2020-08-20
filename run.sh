634a753fdcfe71db34da9611ca6707d65e9bc9ed
Francisco Espinosa <paco.espinosa.pelaez@gmail.com>
2020-08-20 12:47:08 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 cd SWE_solver
 ./compile
 cd ..
-./SWE_solver/build/SWE_gnu_debug_none_fwave_1 -x $size -y $size -t $type -o SWEoutput/$domain1
+./SWE_solver/build/SWE_gnu_debug_none_fwave_1 -x $size -y $size -t $type -o $output
